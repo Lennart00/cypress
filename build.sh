1acd015d395883eb0af5aeff44d147dcd711de7c
Lennart Jaschob <lennart.jaschob@primekey.com>
2021-04-27 16:31:35 +0200

    update script

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-set -ex
-yarn
-yarn build --scope cypress
-yarn binary-build
-yarn binary-zip
+set -x
+
+main(){
+yarn || return 1
+yarn build --scope cypress || return 1
+yarn binary-build || return 1
+yarn binary-zip || return 1
+   cd cli || return 1
+yarn || return 1
+yarn build || return 1
+cd build || return 1
+yarn pack || return 1
+}
+main "$@"
